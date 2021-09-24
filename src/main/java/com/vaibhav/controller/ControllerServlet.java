c9f1eff74920964758ba7d02a8176d1453dfc8a0
vaibhavw12 <vaibhavwabe2000@gmail.com>
2021-09-24 13:36:20 +0530

    added ControllerServlet.java

---
+++ src/main/java/com/vaibhav/controller/ControllerServlet.java
@@ -0,0 +1,10 @@
+package com.vaibhav.controller;
+
+public class ControllerServlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("vaibhav");
+	}
+
+}
